7572c8513e0aa70d444fc92c1aa4ae655b03f386
jeffreytoconnell <jeffreytoconnell@gmail.com>
2017-08-24 14:37:13 -0700

    react_click

---
+++ src/components/surprise.js
@@ -0,0 +1,29 @@
+// THIS GOES TO INDEX.JS
+
+import React from 'react';
+import SurpriseButton from './surprise-button';
+import SurpriseImage from './surprise-image';
+
+export default class Surprise extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            display: 'button'  // INITIALLY WILL DISPLAY 'BUTTON' ??
+        }
+    }
+
+    showImage() {
+        this.setState({
+            display: 'image'  // ??
+        });
+    }
+
+    render() {
+        if (this.state.display === 'button') {
+            return <SurpriseButton onClick={event => this.showImage()}/>;  // FROM ABOVE BUTTON LIVES IN SURPRISEBUTTON
+        }
+        else if (this.state.display === 'image') {
+            return <SurpriseImage />;  // FROM ABOVE
+        }
+    }
+}
